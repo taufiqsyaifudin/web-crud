7fd575c73d0abfda43a28f695ac59b2ad950a6c3
Taufiq Syaifudin <taufiqsyaifudin.dev@gmail.com>
2023-09-13 09:14:08 +0700

    web crud

--- frontend/src/components/AddUser.js
+++ frontend/src/components/AddUser.js
@@ -11,7 +11,7 @@ const AddUser = () => {
   const saveUser = async (e) => {
     e.preventDefault();
     try {
-      await axios.post("http://localhost:5000/users", {
+      await axios.post("https://web-crud-api.vercel.app/users", {
         name,
         email,
         gender,
