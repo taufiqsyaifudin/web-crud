166f8922dbedebf2093c9c519c952633fcb7382e
Taufiq Syaifudin <taufiqsyaifudin.dev@gmail.com>
2023-09-13 09:49:25 +0700

    web crud

--- frontend/src/components/EditUser.js
+++ frontend/src/components/EditUser.js
@@ -14,7 +14,7 @@ const EditUser = () => {
   }, []);
 
   const getUserById = async () => {
-    const response = await axios.get(`https://web-crud-api.vercel.app/${id}`);
+    const response = await axios.get(`https://web-crud-api.vercel.app/users/${id}`);
     setName(response.data.name);
     setEmail(response.data.email);
     setGender(response.data.gender);
@@ -23,7 +23,7 @@ const EditUser = () => {
   const updateUser = async (e) => {
     e.preventDefault();
     try {
-      await axios.patch(`https://web-crud-api.vercel.app/${id}`, {
+      await axios.patch(`https://web-crud-api.vercel.app/users${id}`, {
         name,
         email,
         gender,
